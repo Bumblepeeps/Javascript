eeb2460e8aca8fe0f0cdb887e2a7286933579ab3
Rachel Liebman <rachel.liebman@vsolvit.com>
2020-12-24 15:52:14 -0500

    L10 - Values and Variables, A10

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+console.log('assignment 10:');
+
+let country = 'United States'
+let continent = 'North America'
+let population = 331949138
+
+console.log("I live in the " + country + ", which is in " + continent + " - population: " + population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,20 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+//value is a piece of data, the most fundamental pieces of data in programming
+
+console.log('jonas');
+console.log(23);
+
+let firstName = 'Honas'
+console.log(firstName + " is " + js);
+//CONVENTIONS FOR VARIABLE NAMES
+//camelCase yo.
+//cannot start a variable name w/ a number: 3years
+//cannot contain ampersand... 
+//acceptable: letters, numbers, underscores, dollar sign
+//variables cannot be named using a reserved js keyword, such as 'new', 'function'
+//do not start variable name with an Uppercase Letter, this is reserved for object-oriented programming
+//true constant can be all uppercase, e.g. PI = 3.1415
+//use descriptive variable names, 'myFirstJob' is better than 'job1'
+
