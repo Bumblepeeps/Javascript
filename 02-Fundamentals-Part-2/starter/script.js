101b27db446fec85f83f5c63cd6b6ee4d9c31652
Rachel Liebman <rachel.liebman@vsolvit.com>
2020-12-29 11:03:12 -0500

    L32 - Activating Strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,35 @@
+// //32. Activating strict mode
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense)
+    console.log('I can drive :)')
+
+    //The following will result in -- Uncaught SyntaxError: Unexpected strict mode reserved word
+// const interface = i;
+// const private = p;
+// const if = f;
+
+//*******************************************************
+// // *****************33. Functions*****************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ////////////////////////////////////////////////////////////
+// // 38. *****************CODING CHALLENGE #1*****************
