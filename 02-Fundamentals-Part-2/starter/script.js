744736750f591e1767f01f08bfd464b8846fb8e0
Rachel Liebman <rachel.liebman@vsolvit.com>
2020-12-29 16:22:12 -0500

    L33 - Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,26 +4,45 @@
 let hasDriversLicense = false;
 const passTest = true;
 
-if (passTest) hasDriverLicense = true;
+// if (passTest) hasDriverLicense = true; 
+//will result in: Uncaught ReferenceError: hasDriverLicense is not defined
 if (hasDriversLicense)
     console.log('I can drive :)')
 
-    //The following will result in -- Uncaught SyntaxError: Unexpected strict mode reserved word
+//The following will result in -- Uncaught SyntaxError: Unexpected strict mode reserved word
 // const interface = i;
 // const private = p;
 // const if = f;
 
 //*******************************************************
 // // *****************33. Functions*****************
+//FUNCTION: a piece of code we can reuse, like a variable but for whole chunks of code
+function logger(name, age) {
+    console.log(`My name is ${name}, and I'm ${age} years old. `);
+}
+//calling / running / invoking functions
+logger('bob', 23);
+logger('caroline', 15);
+logger('cindy', 12);
 
+//functions are like machines, like a food processor. 
+//DRY don't repeat yourself. Functions help us keep our code DRY.
 
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice; //becomes the result of running this function
+}
 
+const appleJuice = fruitProcessor(5, 0); //inputs / actual values / arguments 5 & 0.
+console.log(`apple Juice is: ${appleJuice}`);
 
+const orangeJuice = fruitProcessor(0, 3); //orangeJuice value is being assigned the value of 'juice' that is produced by our function, given the parameters 0, 3. 
+console.log(`orange Juice is: ${orangeJuice}`);
 
+const mixedJuice = fruitProcessor(2, 4);
+console.log(`mixed Juice is: ${mixedJuice}`);
 
-
-
-
+const num = Number('22'); // Number is a built-in js function, so is "log"
 
 
 
