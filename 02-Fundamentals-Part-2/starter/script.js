9ae884489d2eda3515388781d155ee3c2f30982b
Rachel Liebman <rachel.liebman@vsolvit.com>
2020-12-31 13:33:03 -0500

    L38 - CODING CHALLENGE #1 - Dolphins and Koalas with Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -67,25 +67,24 @@ console.log(age1, age2); //log the returned value from the function.
 //will learn about hoisting later... impacts when you can call a function. 
 //which type of function should I use? Often based on preference. Expressions require each to be assigned a variable and require being expressed before being called / cleaner code.
 
-/*
-//*******************************************************
-// // *****************35. Arrow Functions*****************
-//Arrow function
-const calcAge3 = birthYear => 2020 - birthYear;
-//easier and faster to write, no need for curly braces to define a code block, return happens implicitly, value is automatically returned without the return line. 
-const age3 = calcAge3(1995);
-console.log(age3);
+// //*******************************************************
+// // // *****************35. Arrow Functions*****************
+// //Arrow function
+// const calcAge3 = birthYear => 2020 - birthYear;
+// //easier and faster to write, no need for curly braces to define a code block, return happens implicitly, value is automatically returned without the return line. 
+// const age3 = calcAge3(1995);
+// console.log(age3);
 
-//how to lose the advantage of the arrow function: multiple lines within function.
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2020 - birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years.`
-}
-console.log(yearsUntilRetirement(1989, 'Rachel'));
-console.log(yearsUntilRetirement(1996, 'Benji'));
-*/
+// //how to lose the advantage of the arrow function: multiple lines within function.
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2020 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years.`
+// }
+// console.log(yearsUntilRetirement(1989, 'Rachel'));
+// console.log(yearsUntilRetirement(1996, 'Benji'));
+
 
 //*******************************************************
 // // *****************36. Functions calling other functions*****************
@@ -125,12 +124,43 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 }
 console.log(yearsUntilRetirement(1985, 'Daniel'));
 console.log(yearsUntilRetirement(1945, 'Mike'));
-
-
-
-
-
-
-
+/**/
 // ////////////////////////////////////////////////////////////
 // // 38. *****************CODING CHALLENGE #1*****************
+
+/*Each team competes 3 times and average is calc'd for each team
+A team ONLY wins if it has DOUBLE or more the average score of the other team. Otherwise, NO team wins.
+
+1. Create an arrow fucntion 'calcAverage' to calc avg of 3 #s (generic)
+2. Use the function to calc average for both teams
+3. Create a function 'checkWinner' to take average score of each team : avgDolphins avgKoalas.
+Function logs the winner to console with the victory points, according to the rules.
+"Koalas win (30 vs. 13)"
+4. checkWinner function called to determine winner for both data1 and data2
+5. Ignore draws this time
+*/
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+
+    if (avgDolphins >= (avgKoalas * 2)) {
+        return `Dolphin Team`;
+    } else if (avgKoalas >= (avgDolphins * 2)) {
+        return `Koala Team`;
+    } else {
+        return `No one`;
+    }
+}
+
+//DATA 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(`Data set 1 Scores -- Dolphins: ${scoreDolphins} Koalas: ${scoreKoalas}`);
+console.log(`${checkWinner(scoreDolphins, scoreKoalas)} wins! ${scoreDolphins} to ${scoreKoalas}`);
+
+//DATA 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+// console.log(`Data set 2 Scores -- Dolphins: ${scoreDolphins} Koalas: ${scoreKoalas}`);
+
+console.log(`${checkWinner(scoreDolphins, scoreKoalas)} wins! ${scoreDolphins} to ${scoreKoalas}`);
