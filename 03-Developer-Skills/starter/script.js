719518ea1d7d636286e9b57c716384c86c13bff9
rachel liebman <rlieb002@gmail.com>
2021-01-31 23:05:35 -0500

    settings, extensions, installs, notes

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,4 +5,27 @@ const x = 23;
 const calcAge = birthYear => 2037 - birthYear;
 console.log(calcAge(1991));
 //Dev Skills Notes
+const cl = console.log();
+/*settings
+Auto Save - onFocusChange
+Multi Cursor Modifier - alt
+Word Wrap - on
+Format On Save - checked
+*/
 
+/*extensions
+Monokai Pro
+Auto Close Tag
+Auto Rename Tag
+ESLint
+Image preview
+Settings Sync
+TODO highlight
+Live Server
+
+/*installs
+node.js
+run: node -v
+run: npm install live-server -g
+run: live-server
+*/
