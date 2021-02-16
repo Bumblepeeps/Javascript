fc0207f0670db672408d2db566093097e5fe97d7
Rachel Liebman <rachel.liebman@vsolvit.com>
2021-02-16 14:56:40 -0500

    L70 - PROJECT: Guess my Number - introduction. L71 - DOM Manipulation Notes.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+//PROJECT 1 INTRO: GUESS MY NUMBER!
+console.log(document.querySelector('.message').textContent);
+//DOM MANIPULATION NOTES
+/* Notes *********************
+- Document Object Model
+
+structured representation of html docs. DOM is connection point between HTML and JS. 
+DOM automatically created by browser - stored in tree stucture. 
+document.querySelector() is a DOM thing, not a JS thing. 
+
+DOM and DOM methods are part of Web APIs
+ - Web API: libraries that browsers implement, accessible by our JS, and also written in JS. 
+ - Web APIs contain: DOM & DOM methods, Fetch API, Timers... etc.
+
+API = Application Programming Interface
+official DOM specification browsers implement, means DOM manipulation works in all browsers. 
+*/
